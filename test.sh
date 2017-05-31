efbe7220d5b20a2913bfcb92d3f10c1ac750b093
Kevin Lyda <kevin@ie.suberic.net>
2017-05-31 09:57:35 +0100

    Got an example repo.

--- test.sh
+++ test.sh
@@ -2,9 +2,12 @@
 
 if [ ! -d mirrors ]; then
   mkdir mirrors
-  git clone --mirror \
-    git://github.com/isagalaev/highlight.js.git \
-    mirrors/highlight.js.git
-  GIT_DIR=./mirrors/highlight.js.git git gc --aggressive
+  #git clone --mirror \
+  #  git://github.com/isagalaev/highlight.js.git \
+  #  mirrors/highlight.js.git
+  #GIT_DIR=./mirrors/highlight.js.git git gc --aggressive
+  curl -L \
+    'https://www.dropbox.com/s/p7smueklddt8bws/highlight.js.git.tar?dl=0' \
+    | tar xf - -C mirrors
 fi
 go run repo.go mirrors
